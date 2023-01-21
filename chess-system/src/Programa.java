f3b094fb5e5ff08a13728962624b064929fad4f4
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-01-21 11:35:05 -0300

    Primeira classe: Posicao

---
+++ chess-system/src/Programa.java
@@ -0,0 +1,9 @@
+import tabuleiro.Posicao;
+
+public class Programa {
+    public static void main(String[] args) throws Exception {
+      
+        Posicao pos = new Posicao(3, 5);
+        System.out.println(pos);
+    }
+}
