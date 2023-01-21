d36416d6386600377e0fe4ad1182ec679d317625
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-01-21 14:16:57 -0300

    Começando a implementar Tabuleiro e Peça

--- chess-system/src/Programa.java
+++
@@ -1,9 +0,0 @@
-import tabuleiro.Posicao;
-
-public class Programa {
-    public static void main(String[] args) throws Exception {
-      
-        Posicao pos = new Posicao(3, 5);
-        System.out.println(pos);
-    }
-}
---
+++ chess-system/src/application/Programa.java
@@ -0,0 +1,11 @@
+package application;
+
+import boardgame.Posicao;
+import boardgame.Tabuleiro;
+
+public class Programa {
+    public static void main(String[] args) throws Exception {
+
+        Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+    }
+}
